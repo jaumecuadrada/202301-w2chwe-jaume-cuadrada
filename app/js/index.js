e68b48b8e7212b50ed328cb2f703c2b251d4d797
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-01-23 00:16:32 +0100

    Add function buttons and style

--- app/js/index.js
+++ app/js/index.js
@@ -5,12 +5,31 @@ import nextField from "./nextField/nextField.js";
 
 const cells = [];
 const html = [];
+const startGameButton = document.querySelector(".start");
+const stopGameButton = document.querySelector(".stop");
+const resumeGameButton = document.querySelector(".resume");
+const randomizeButton = document.querySelector(".randomize");
+let interval;
 
-const startGame = () => {
-  createField(cells, html);
+const initializeGame = () => {
   randomize(cells);
   draw(cells, html);
-  setInterval(nextField, 100, cells, html);
 };
 
-startGame();
+const startGame = () => {
+  interval = setInterval(nextField, 100, cells, html);
+};
+
+const stopGame = () => {
+  clearInterval(interval);
+};
+
+createField(cells, html);
+initializeGame();
+
+startGameButton.addEventListener("click", startGame);
+stopGameButton.addEventListener("click", stopGame);
+resumeGameButton.addEventListener("click", startGame);
+randomizeButton.addEventListener("click", () => {
+  location.reload();
+});
